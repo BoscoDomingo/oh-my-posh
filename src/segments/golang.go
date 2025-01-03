9b760b26a29d324fa578632a7cca5fefd92cc9d7
Bosco Domingo <46006784+BoscoDomingo@users.noreply.github.com>
2025-01-03 17:20:02 +0100

    feat: enable Go with go.work
    go.work is a file that signifies a monorepo, and may be used to determine if Go should be enabled

--- src/segments/golang.go
+++ src/segments/golang.go
@@ -19,7 +19,7 @@ func (g *Golang) Template() string {
 }
 
 func (g *Golang) Enabled() bool {
-	g.extensions = []string{"*.go", "go.mod"}
+	g.extensions = []string{"*.go", "go.mod", "go.work"}
 	g.commands = []*cmd{
 		{
 			regex:      `(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+)(.(?P<patch>[0-9]+))?))`,
